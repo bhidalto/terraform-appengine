b7bd697ee38dbd361260655d66a10e335b4aa5ef
Borja <borjah@google.com>
2020-10-20 14:50:09 +0000

    ran terraform fmt on all but standard modules

--- modules/basic_scaling/main.tf
+++ modules/basic_scaling/main.tf
@@ -1,4 +1,4 @@
 resource "basic_scaling_settings" "basic_scaling" {
-    idle_timeout = var.idle_timeout
-    max_instances = var.max_instances
+  idle_timeout  = var.idle_timeout
+  max_instances = var.max_instances
 }
--- modules/manual_scaling/variables.tf
+++ modules/manual_scaling/variables.tf
@@ -1,10 +1,10 @@
 variable "instances" {
-    description = "The number of instances to create for the Service. Defaults to 1."
-    type = number
-    default = 1
+  description = "The number of instances to create for the Service. Defaults to 1."
+  type        = number
+  default     = 1
 
-    validation {
-        condition = var.instances > 0
-        error_message = "You must allocate at least 1 instance."
-    }
+  validation {
+    condition     = var.instances > 0
+    error_message = "You must allocate at least 1 instance."
+  }
 }
