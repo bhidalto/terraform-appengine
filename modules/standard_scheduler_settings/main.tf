4a0b1dc6153e049742b148fe8f3fd75efe0f5e1d
Borja <borjah@google.com>
2020-10-20 13:40:15 +0000

    created submodule standard_scheduler_settings needed for automatic_scaling

---
+++ modules/standard_scheduler_settings/main.tf
@@ -0,0 +1,6 @@
+resource "standard_scheduler_settings" "standard_scheduler_settings" {
+    target_cpu_utilization = var.target_cpu_utilization
+    target_throughput_utilization = var.target_throughput_utilization
+    min_instances = var.min_instances
+    max_instances = var.max_instances
+}
---
+++ modules/standard_scheduler_settings/variables.tf
@@ -0,0 +1,43 @@
+variable "target_cpu_utilization" {
+    description = "Target CPU utilization ratio to maintain when scaling. When the CPU utilisation bypasses this threshold, a new instance will be spin up. Defaults to 0.6" 
+    type = number
+    default = 0.6
+
+    validation {
+        condition = var.target_cpu_utilization >= 0.5 && var.target_cpu_utilization <= 0.95
+        error_message = "The target_cpu_utilization value must fall within range [0.5, 0.95]"
+    }
+}
+
+variable "target_throughput_utilization" {
+    description = "Target throughput utilization ratio to maintain when scaling. When the target_throughput_utilization threshold is bypassed, a new instance is spun up. Defaults to 0.6"
+    type = number
+    default = 0.6
+    
+    validation {
+        condition = var.target_throughput_utilization >= 0.5 && var.target_throughput_utilization <= 0.95
+        error_message = "The target_throughput_utilization value must fall within range [0.5, 0.95]"
+    }
+}
+
+variable "min_instances" {
+    description = " Minimum number of instances to run for this version. Value can be set to 0 for the system to downscale in case of no traffic being served. Defaults to 0."
+    type = number
+    default = 0
+
+    validation {
+        condition = var.min_instances >= 0 && var.min_instances <= 1000
+        error_message = "The min_instances value must fall within range [0,1000]"
+    }
+}
+
+variable "max_instances" {
+    description = "Maximum number of instances to run for this version. Specifying a value of 0 disables the setting. Defaults to 0."
+    type = number
+    default = 0
+
+    validation {
+        condition = var.max_instances >= 0 && var.max_instances <= 2147483647
+        error_message = "The max_instances value must fall within range [0,2147483647]"
+    }
+}
