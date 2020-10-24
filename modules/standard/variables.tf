e670a3c7c5bad241ba687c3620a434c6274907e3
Borja <borjah@google.com>
2020-10-24 10:04:00 +0000

    appengine landing readme prepared

--- modules/standard/variables.tf
+++ modules/standard/variables.tf
@@ -84,9 +84,7 @@ variable "inbound_services" {
 }
 
 variable "project" {
-  description = "(Optional) The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
-  type        = string
-  default     = null
+
 }
 
 variable "deployment" {
